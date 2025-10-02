9dc27c0a04c6c969152db1d2ef78e055dcac151f
Pedro Henrique Lopes De Melo <pedrolopesmelo18@gmail.com>
2025-10-02 18:59:19 -0300

    .

--- 2°periodo/AEDS2/lab/tps/tp04/java/Main.java
+++ 2°periodo/AEDS2/lab/tps/tp04/java/Main.java
@@ -70,8 +70,93 @@ public void setTags(String[] tags) {
 
 public class Main{
 
-    public static void set(Game game,String[] atributos){
+    public static void formatar(Game game, String linha){
+        int i = 0;
+        int contagemVirgulas = 0;
+        String auxId = "";
+        while(linha.charAt(i) != ','){
+            i++;
+        }
+        contagemVirgulas++;
+        for(int u = 0; u < i; u++){
+            auxId += linha.charAt(u);
+        }
+        int id = Integer.parseInt(auxId);
+        game.setId(id);
+         
+        String auxName = "";
+        for(int u = i+1; linha.charAt(u) != ','; u++){
+               auxName += linha.charAt(u);
+        }
+        game.setName(auxName);
+        contagemVirgulas++;
+         String auxreleaseDate = "";
+         int contagem = 0;
+         for(int u = 0; u < linha.length(); u++){
+            if(linha.charAt(u) == ','){
+                contagem++;
+            }
+            if(contagem >= 2 && contagem < 4){
+                if(linha.charAt(u) != '"'){
+                    auxreleaseDate += linha.charAt(u);
+                }
+            }
+         }
+         game.setReleaseDate(auxreleaseDate);
+         contagemVirgulas= 4;
+         contagem = 0;
+         String auxestimatedOwners = "";
+         for(int u = 0; u < linha.length(); u++){
+            if(linha.charAt(u) == ','){
+                contagem ++;
+            }
+            if(contagem == contagemVirgulas){
+                auxestimatedOwners += linha.charAt(u+1);
+            }
+         }
+           int estimatedOwners = Integer.parseInt(auxestimatedOwners);
+           game.setEstimatedOwners(estimatedOwners);
+           contagemVirgulas++;
+           contagem = 0;
 
+           String auxPrice = "";
+           for(int u = 0; u < linha.length(); u++){
+            if(linha.charAt(u) == ','){
+                contagem ++;
+            }
+            if(contagem == contagemVirgulas){
+                auxPrice += linha.charAt(u+1);
+            }
+         }
+         float price = Float.parseFloat(auxPrice);
+         game.setPrice(price);
+
+         contagemVirgulas++;
+         contagem = 0;
+         int contagemAspas = 0;
+
+         String auxsupportedLanguages = "";
+         for(int u = 0; u < linha.length(); u++){
+            if(contagem < contagemVirgulas){
+            if(linha.charAt(u) == ','){
+                contagem ++;
+            }
+        }
+        if(contagem == contagemVirgulas && linha.charAt(u) == '"'){
+            contagemAspas++;
+        }
+        if(contagemAspas < 2){
+            if(contagem == contagemVirgulas && linha.charAt(u) != '"' && linha.charAt(u) != '\''){
+                auxsupportedLanguages += linha.charAt(u);
+            }
+         }
+        }
+
+            
+            
+        }
+        
+         
     }
 
     public static void main(String[] args){
@@ -84,8 +169,8 @@ public class Main{
           
           while(scFile.hasNext()){
             game[i] =  new Game();
-            String[] atributos = new String[14];
-            set(game[i], atributos);
+            String linha = scFile.nextLine();
+            formatar(game[i],linha);
             i++;  
           }
         
